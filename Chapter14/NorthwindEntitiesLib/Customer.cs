1a77d7b0540e7f968e1e37e8f52abfb4ae07e73a
Mark J Price <markjprice@gmail.com>
2018-12-29 13:05:20 +0000

    Initial commit

---
+++ Chapter14/NorthwindEntitiesLib/Customer.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace Packt.Shared
+{
+  public class Customer
+  {
+    public string CustomerID { get; set; }
+    public string CompanyName { get; set; }
+    public string ContactName { get; set; }
+    public string ContactTitle { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+    public string Fax { get; set; }
+
+    // related entities
+    public ICollection<Order> Orders { get; set; }
+  }
+}
---
+++ Chapter14/NorthwindWeb/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore; 
+using Packt.Shared;
+
+namespace NorthwindWeb
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+
+      services.AddDbContext<Northwind>(options => 
+        options.UseSqlite("Data Source=Northwind.db"));
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      // app.Run(async (context) =>
+      // {
+      //   await context.Response.WriteAsync("Hello World!");
+      // });
+
+      app.UseDefaultFiles(); // index.html, default.html, and so on
+      app.UseStaticFiles();
+      app.UseMvc(); // includes the Razor Pages feature
+    }
+  }
+}
